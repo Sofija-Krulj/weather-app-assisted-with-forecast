04aa3baf2d3b8bf83301e9b5d0e4424f4741e613
Sofija-Krulj <sofija.krulj@gmail.com>
2025-05-09 14:29:43 +0200

    Add function displayTemperature
    Fetch the response weather-app-api data

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-function displayTemperature() {}
+function displayTemperature(response) {
+  temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temmperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
 
 function searchCity(city) {
   apiKey = "o3cf1124f7350046738b1c34ad3dt312";
-  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -12,6 +16,7 @@ function searchEngine(event) {
 
   let searchInput = document.querySelector("#search-form-input");
   let heading = document.querySelector("h1");
+  heading.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
