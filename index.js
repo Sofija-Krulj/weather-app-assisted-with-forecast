3c9153d75b09c735dead55451d87a423c9b7819a
Sofija-Krulj <sofija.krulj@gmail.com>
2025-05-09 14:08:27 +0200

    Add function searchCity
    This function is ment to display the searched city

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+function displayTemperature() {}
+
+function searchCity(city) {
+  apiKey = "o3cf1124f7350046738b1c34ad3dt312";
+  apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
 function searchEngine(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
   let heading = document.querySelector("h1");
-  heading.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let form = document.querySelector("#main-form");
